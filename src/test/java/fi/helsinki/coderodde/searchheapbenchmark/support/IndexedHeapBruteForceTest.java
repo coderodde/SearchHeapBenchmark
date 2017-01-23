36b874638d205ef5cf37686700a9a1fe052eccd1
coderodde <rodion.efremov@cs.helsinki.fi>
2017-01-23 12:54:13 +0200

    Looks good.

--- src/test/java/fi/helsinki/coderodde/searchheapbenchmark/support/IndexedHeapBruteForceTest.java
+++ src/test/java/fi/helsinki/coderodde/searchheapbenchmark/support/IndexedHeapBruteForceTest.java
@@ -14,7 +14,7 @@ public class IndexedHeapBruteForceTest {
     
     @Test
     public void test() {
-        long seed = 1484998845800L; System.currentTimeMillis();
+        long seed = System.currentTimeMillis();
         Random random = new Random(seed);
         System.out.println("IndexedHeapCorrectnessTest seed = " + seed);
         List<HeapTask> heapTaskList = getRandomHeapTaskList(OPERATIONS_PER_HEAP,
@@ -33,43 +33,43 @@ public class IndexedHeapBruteForceTest {
         List<Integer> resultListOfBinaryHeap = test(binaryHeap, 
                                                     heapTaskList,
                                                     new Random(seed));
-//        
-//        List<Integer> resultListOfDaryHeap2 = test(d2aryHeap,
-//                                                   heapTaskList,
-//                                                   new Random(seed));
-//        
-//        List<Integer> resultListOfDaryHeap3 = test(d3aryHeap,
-//                                                   heapTaskList,
-//                                                   new Random(seed));
-//        
-//        List<Integer> resultListOfDaryHeap4 = test(d4aryHeap,
-//                                                   heapTaskList,
-//                                                   new Random(seed));
-//        
-//        List<Integer> resultListOfDaryHeap5 = test(d5aryHeap,
-//                                                   heapTaskList,
-//                                                   new Random(seed));
-//        
+        
+        List<Integer> resultListOfDaryHeap2 = test(d2aryHeap,
+                                                   heapTaskList,
+                                                   new Random(seed));
+        
+        List<Integer> resultListOfDaryHeap3 = test(d3aryHeap,
+                                                   heapTaskList,
+                                                   new Random(seed));
+        
+        List<Integer> resultListOfDaryHeap4 = test(d4aryHeap,
+                                                   heapTaskList,
+                                                   new Random(seed));
+        
+        List<Integer> resultListOfDaryHeap5 = test(d5aryHeap,
+                                                   heapTaskList,
+                                                   new Random(seed));
+        
         List<Integer> resultListOfBinomialHeap = test(binomialHeap,
                                                       heapTaskList,
                                                       new Random(seed));
         
-//        List<Integer> resultListOfFibonacciHeap = test(fibonacciHeap,
-//                                                       heapTaskList,
-//                                                       new Random(seed));
+        List<Integer> resultListOfFibonacciHeap = test(fibonacciHeap,
+                                                       heapTaskList,
+                                                       new Random(seed));
         
         Map<Integer, Integer> map1 = binaryHeap.getPriorityMap();
         Map<Integer, Integer> map2 = binomialHeap.getPriorityMap();
         assertTrue(map1.equals(map2));
         
         boolean allEqual = 
-                listsEqual(resultListOfBinaryHeap,/*
+                listsEqual(resultListOfBinaryHeap,
                            resultListOfDaryHeap2,
                            resultListOfDaryHeap3,
                            resultListOfDaryHeap4,
-                           resultListOfDaryHeap5,*/
-                           resultListOfBinomialHeap/*,
-                           resultListOfFibonacciHeap*/);
+                           resultListOfDaryHeap5,
+                           resultListOfBinomialHeap,
+                           resultListOfFibonacciHeap);
         
         assertTrue(allEqual);
     }
